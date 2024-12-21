ef216ee32685293f4958b80d2f4a045c83fbb627
yanshuman <yanshuman2005@gmail.com>
2024-12-21 22:32:48 +0530

    Add ScopeTest component with scoped styles and complex animation

--- src/pages/Playground/Playground.tsx
+++ src/pages/Playground/Playground.tsx
@@ -1,9 +1,11 @@
 import Expandable from "./sections/Expandable";
+import ScopeTest from "./sections/ScopeTest";
 
 export default function Playground() {
   return (
     <>
       <Expandable />
+      <ScopeTest />
     </>
   );
 }
---
+++ src/pages/Playground/sections/ScopeTest.tsx
@@ -0,0 +1,28 @@
+const css = String.raw;
+
+export default function ScopeTest() {
+  const scopedStyle = css`
+    @scope {
+      .local-div {
+        background: hsla(90, 80%, 90%, 20%);
+        width: 200px;
+        border: 2px solid rebeccapurple;
+        animation: someComplexAnimation 3s infinite;
+      }
+    }
+  `;
+
+  return (
+    <>
+      <section className="mt-32">
+        <style>{scopedStyle}</style>
+        <div className="local-div mx-auto">ScopeTest text 1</div>
+      </section>
+      <section className="mt-32">
+        <div className="local-div mx-auto">
+          ScopeTest text 2 <i>this div also has the class local-div</i>
+        </div>
+      </section>
+    </>
+  );
+}
