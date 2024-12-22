ba7c10c3e8a1183004c80bf0064f4ffab2fc920c
Devanshnair <144161376+Devanshnair@users.noreply.github.com>
2024-12-22 13:52:48 +0530

    Add tailwind responsive breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
+        screens: {
+            'xs': '400px',
+            'xs2': '500px',
+            'sm': '640px',
+            'md': '768px',
+            'md2': '900px',
+            'lg': '1024px',
+            'lg2': '1100px',
+            'xl': '1280px',
+            '2xl': '1536px',
+          },
         extend: {},
     },
     plugins: [],
